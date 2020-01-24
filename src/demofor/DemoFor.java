dc11dbda56bd2b23ec7d4c6c440707af862bb3b4
kaur7301 <ramanpreetk514@gmail.com>
2020-01-24 09:31:45 -0500

    three

---
+++ src/demofor/DemoFor.java
@@ -0,0 +1,34 @@
+package demofor;
+
+public class DemoFor {
+
+    public static void main(String[] args) {
+  
+        
+        for(int x=0; x<5; x++)
+        System.out.println("x is: "+x);
+        
+         for(int x=1; x<5; x++)
+        System.out.println("x is: "+x);
+       
+       
+        for(int x=4; x>=0; x--)
+        System.out.println("x is: "+x);
+        
+        for(int x=0; x<5; x++)
+            if(x!=2)
+        
+        System.out.println("x is: "+x);
+        
+        for(int x=1; x<10; x++)
+            if(x<3 ||x>5)
+                
+        System.out.println("x is: "+x);
+        
+       
+       
+       
+        
+    }
+    
+}
